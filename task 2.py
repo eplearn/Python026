5872869245d88bffd8d22824c5c0fda2db703b4d
Ev Paul <git@github.com:eplearn/frontend.git>
2021-07-03 00:50:18 +0400

    Bug is fixed - find_numbers(path, lock) is locking now.

--- task 2.py
+++ task 2.py
@@ -14,8 +14,8 @@ def main():
     lock = threading.Lock()
 
     filling_thread = threading.Thread(target=random_fill, args=(path, lock))
-    primes_thread = threading.Thread(target=calc_primes, args=(path,))
-    factorials_thread = threading.Thread(target=calc_factorials, args=(path,))
+    primes_thread = threading.Thread(target=calc_primes, args=(path, lock))
+    factorials_thread = threading.Thread(target=calc_factorials, args=(path, lock))
 
     filling_thread.start()
     primes_thread.start()
@@ -34,22 +34,24 @@ def input_path():
 
 def random_fill(path, lock):
     with lock:
+        print('filling is started')
         data_length = 100
         data = []
         for i in range(data_length):
             data.append(rand.randint(1, 1000))
-
         with path.open(mode='w') as file:
             file.write(str(data))
+        print('filling is finished')
 
 
-def find_numbers(path):
-    numbers = []
-    with path.open(mode='r') as file:
-        for line in file:
-            nums = re.findall(r'\d+', line.strip('\n'))
-            numbers.extend(nums)
-    return list(map(int, numbers))
+def find_numbers(path, lock):
+    with lock:
+        numbers = []
+        with path.open(mode='r') as file:
+            for line in file:
+                nums = re.findall(r'\d+', line.strip('\n'))
+                numbers.extend(nums)
+        return list(map(int, numbers))
 
 
 def find_primes(numbers):
@@ -71,23 +73,29 @@ def check_prime(number):
         return False
 
 
-def calc_primes(path):
-    print(f'numbers {find_numbers(path)}')
-    primes = find_primes(find_numbers(path))
-    print(f'primes {primes}')
+def calc_primes(path, lock):
+    print('find_numbers is started in calc_primes')
+    # print(f'numbers {find_numbers(path, lock)}')
+    primes = find_primes(find_numbers(path, lock))
+    print('find_numbers is finished in calc_primes')
+    # print(f'primes {primes}')
     with open('primes.txt', 'w') as f:
         f.write(str(primes))
+    # print('calc_primes is finished')
 
 
-def calc_factorials(path):
-    print(f'numbers {find_numbers(path)}')
-    numbers = find_numbers(path)
+def calc_factorials(path, lock):
+    print('find_numbers is started in calc_factorials')
+    # print(f'numbers {find_numbers(path)}')
+    numbers = find_numbers(path, lock)
+    print('find_numbers is finished in calc_factorials')
     factorials = []
     for number in numbers:
         factorials.append(math.factorial(number))
-    print(f'factorials {factorials}')
+    # print(f'factorials {factorials}')
     with open('factorials.txt', 'w') as f:
         f.write(str(factorials))
+    # print('calc_factorials is finished')
 
 
 if __name__ == '__main__':
