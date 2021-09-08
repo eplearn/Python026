bd3f910c0a57df2a82bf79f4cfb63596e8246293
Ev Paul <git@github.com:eplearn/frontend.git>
2021-09-08 21:02:57 +0400

    somewhat

--- Python026/shop/website/urls.py
+++ Python026/shop/website/urls.py
@@ -1,6 +1,23 @@
 from django.urls import path
 from . import views
 
-urplpatterns = [
-    path('', views.index)
+# from django.contrib import admin
+# from django.urls import path, include
+#
+# urlpatterns = [
+#     # path('admin', admin.site.urls),
+#     path('', include('website.urls'))
+# ]
+
+
+from django.contrib import admin
+
+from django.urls import path, include
+
+urlpatterns = [
+
+    # path('admin/', admin.site.urls),
+
+    path('', include("website.urls"))
+
 ]
