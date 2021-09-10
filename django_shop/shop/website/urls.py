068b8790fb9be70e8e91c96bc0d23fb3a399641e
Ev Paul <git@github.com:eplearn/frontend.git>
2021-09-11 02:33:47 +0400

    It is possible to add and remove product.

--- django_shop/shop/website/urls.py
+++ django_shop/shop/website/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'website'
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index'),
+    path('about', views.about),
+    path('create', views.create),
+    path('delete/<int:id>', views.delete)
 ]
--- django_shop/shop/website/views.py
+++ django_shop/shop/website/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Product
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse(1)
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'website/index.html', context)
+
+
+def about(request):
+    return render(request, 'website/about.html')
+
+
+def create(request):
+    # Необходима валидация.
+    if request.method == 'POST':
+        product = Product(
+            name=request.POST.get('name'),
+            description=request.POST.get('description'),
+            price=request.POST.get('price')
+        )
+        product.save()
+    return render(request, 'website/create.html')
+
+
+def delete(request, id):
+    Product.objects.filter(id=id).delete()
+    return redirect('website:index')
