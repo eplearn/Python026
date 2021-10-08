7f67739071c4e52c649558bfdb40df7e820a2d34
Ev Paul <git@github.com:eplearn/frontend.git>
2021-10-09 01:04:36 +0400

    Admin panel settings.

--- shop/website/admin.py
+++ shop/website/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import NoteModel
 
+
+class NoteAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'category', 'abstract', 'text')
+    list_display = ('id', 'is_published', 'author', 'title', 'category', 'publish_date', 'abstract', 'text')
+    list_display_links = ('id', 'title',)
+    list_editable = ('author', 'category', 'is_published', 'abstract', 'text')
+
+
 # Register your models here.
-admin.site.register(NoteModel)
+admin.site.register(NoteModel, NoteAdmin)
