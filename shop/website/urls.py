6fa8407f06708e58acd0fe26f4a8b1308c0ff72d
Ev Paul <git@github.com:eplearn/frontend.git>
2021-10-09 06:46:27 +0400

    Simple refactor is done.

--- shop/website/urls.py
+++ shop/website/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from . import views
 from .views import CreateNote, NotesView, DltNote, ShowNote, RegisterUser, LoginUser, logout_user, UserProfile
 
 app_name = 'website'
